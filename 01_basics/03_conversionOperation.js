bad32619b8dbedea48c3f0964252de9245d0197c
Nitesh NK Chaurasia <niteshbth97@gmail.com>
2026-01-31 02:46:20 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "Nitesh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not and Number) but types Of NaN is Number
+// true => 1; false => 0
+
+let isLoggedIn = "Nitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Nitesh" => true
+
+let someNumber = 99
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
